8f6aa78d1ba5d9567b5f76dd22859a14a2e907df
nikhil nayyar <nikhil.nayyar@classplus.co>
2022-08-24 13:04:53 +0530

    add PKCE

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import * as dotenv from "dotenv";
 import nodeFetch from "node-fetch";
 import express from "express";
+import crypto from "crypto";
+import base64url from "base64url";
 
 dotenv.config();
 
 const { clientID, clientSecret, redirect_uri, serverPort: port } = process.env;
 
 const app = express();
+
+const verifier = base64url(crypto.pseudoRandomBytes(32));
+const challenge = base64url(
+  crypto.createHash("sha256").update(verifier).digest()
+);
+const challengeMethod = "S256";
 
 app.get("/", async (req, res) => {
   const code = req.query.code;
@@ -29,6 +37,7 @@ app.get("/", async (req, res) => {
         grant_type: "authorization_code",
         code: code,
         redirect_uri: redirect_uri,
+        code_verifier: verifier,
       }),
     });
 
@@ -40,7 +49,7 @@ app.get("/", async (req, res) => {
   // Step 2:
   // If no authorization code is available, redirect to Zoom OAuth to authorize
   return res.redirect(
-    `https://zoom.us/oauth/authorize?response_type=code&redirect_uri=${redirect_uri}&client_id=${clientID}`
+    `https://zoom.us/oauth/authorize?response_type=code&redirect_uri=${redirect_uri}&client_id=${clientID}&code_challenge=${challenge}&code_challenge_method=${challengeMethod}`
   );
 });
 
