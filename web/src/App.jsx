75ed1546ba6f4e90beff4ec2f0bd9c4b6ebb4bbc
nikhil nayyar <nikhil.nayyar@classplus.co>
2022-09-22 12:59:31 +0530

    add routes for component and client view

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,8 +1,9 @@
-import { useCallback, useEffect, useState } from "react";
-import ClientView from "./ClientView";
-// import ComponentView from "./ComponentView";
+import { lazy, Suspense, useCallback, useEffect, useState } from "react";
+import ComponentView from "./ComponentView";
 import { checkOauthSetup, joinMeeting } from "./handler";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+const ClientView = lazy(() => import("./ClientView"));
 
 const isOauthSetup = async () => {
   if (!(await checkOauthSetup())) window.location.href = `http://localhost:4000/oauth${window.location.search}`;
@@ -13,41 +14,45 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [meetingData, setMeetingData] = useState(null);
   const [host, setHost] = useState(false);
+  const navigate = useNavigate(false);
 
   const joinMeet = useCallback(async (host) => {
     setMeetingData(await joinMeeting(host));
     setHost(host);
-  }, []);
+    navigate("/client");
+  }, [navigate]);
 
   useEffect(() => {
     isOauthSetup().then((t) => setLoading(!t));
   }, []);
 
   return (
-    <Routes>
-      <Route
-        index
-        element={
-          <div className="App">
-            {!(loading || meetingData) && (
-              <div style={{ position: "fixed", top: "0", left: "0", zIndex: "1000" }}>
-                <button
-                  style={{ padding: "1rem", background: "wheat", marginRight: "1rem" }}
-                  onClick={() => joinMeet(true)}
-                >
-                  Start
-                </button>
-                <button style={{ padding: "1rem", background: "wheat" }} onClick={() => joinMeet(false)}>
-                  Join
-                </button>
-              </div>
-            )}
-            {/* <ComponentView meetingData={meetingData} /> */}
-            <ClientView meetingData={meetingData} host={host} />
-          </div>
-        }
-      />
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route
+          index
+          element={
+            <div className="App">
+              {!(loading || meetingData) && (
+                <div style={{ position: "fixed", top: "0", left: "0", zIndex: "1000" }}>
+                  <button
+                    style={{ padding: "1rem", background: "wheat", marginRight: "1rem" }}
+                    onClick={() => joinMeet(true)}
+                  >
+                    Start
+                  </button>
+                  <button style={{ padding: "1rem", background: "wheat" }} onClick={() => joinMeet(false)}>
+                    Join
+                  </button>
+                </div>
+              )}
+            </div>
+          }
+        />
+        <Route path="client" element={<ClientView meetingData={meetingData} host={host} />} />
+        <Route path="component" element={<ComponentView meetingData={meetingData} />} />
+      </Routes>
+    </Suspense>
   );
 }
 
